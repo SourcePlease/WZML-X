b552b29b00324aea3d2660b6847de6c51818edcf
SourcePlease <134805321+SourcePlease@users.noreply.github.com>
2026-05-12 18:48:53 +0630

    Update start buttons to new labels

--- bot/helper/languages/en.py
+++ bot/helper/languages/en.py
@@ -2,5 +2,5 @@ START_MSG = """
 This bot can mirror from links|tgfiles|torrents|nzb|rclone-cloud to any rclone cloud, Google Drive or to telegram.
 Type /{cmd} to get a list of available commands
 """
-START_BUTTON1 = "Git Repo"
-START_BUTTON2 = "Updates"
+START_BUTTON1 = "𝘚𝘰𝘶𝘳𝘤𝘦 𝘗𝘭𝘦𝘢𝘴𝘦 𝘉𝘰𝘵𝘴"
+START_BUTTON2 = "𝘏𝘶𝘣 𝘖𝘳𝘨𝘢𝘯𝘪𝘻𝘢𝘵𝘪𝘰𝘯"
